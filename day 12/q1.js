78cd6e51bd79052586fc9f1b016b0922eb94f71b
Ayush Mohanty <cse.24bcsi23@silicon.ac.in>
2026-02-18 09:48:26 +0530

    day 12

---
+++ day 12/q1.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+<script>
+document.write("Hello world");
+
+let a=parseInt(prompt("Enter the first number"));
+let b=parseInt(prompt("Enter the second number"));
+
+document.write(a+b);
+</script>
+</head>
+<body>
+</body>
